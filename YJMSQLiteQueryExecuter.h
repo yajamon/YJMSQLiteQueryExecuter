0398db1d076d0deefef5054bf8352db7a50d425e
yajamon <yajamon.tatsuki@gmail.com>
2015-08-06 20:48:19 +0900

    Add base file

---
+++ YJMSQLiteQueryExecuter.h
@@ -0,0 +1,13 @@
+//
+//  YJMSQLiteQueryExecuter.h
+//  Demo
+//
+//  Created by yajamon on 2015/08/06.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJMSQLiteQueryExecuter : NSObject
+
+@end
---
+++ YJMSQLiteQueryExecuter.m
@@ -0,0 +1,13 @@
+//
+//  YJMSQLiteQueryExecuter.m
+//  Demo
+//
+//  Created by yajamon on 2015/08/06.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import "YJMSQLiteQueryExecuter.h"
+
+@implementation YJMSQLiteQueryExecuter
+
+@end
