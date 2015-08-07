5a4ffe59684c0475048e6deac094b06278dcb22f
yajamon <yajamon.tatsuki@gmail.com>
2015-08-07 10:25:36 +0900

    Add base file

---
+++ YJMSQLiteNamedParameter.h
@@ -0,0 +1,13 @@
+//
+//  YJMSQLiteNamedParameter.h
+//  Demo
+//
+//  Created by yajamon on 2015/08/07.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJMSQLiteNamedParameter : NSObject
+
+@end
---
+++ YJMSQLiteNamedParameter.m
@@ -0,0 +1,13 @@
+//
+//  YJMSQLiteNamedParameter.m
+//  Demo
+//
+//  Created by yajamon on 2015/08/07.
+//  Copyright (c) 2015年 yajamon. All rights reserved.
+//
+
+#import "YJMSQLiteNamedParameter.h"
+
+@implementation YJMSQLiteNamedParameter
+
+@end
